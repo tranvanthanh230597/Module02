0657363ddba867afbc3c4ae191b6cc907f9665bd
tranvanthanh230597 <tranvanthanh230597@gmail.com>
2019-10-11 17:05:52 +0700

    [Thực hành] Ứng dụng tính tiền lãi cho vay

---
+++ src/Basic_java/Practice/interset.java
@@ -0,0 +1,25 @@
+package Basic_java.Practice;
+
+import java.util.Scanner;
+
+public class interset {
+    public static void main(String[] args) {
+        double money;
+        int month;
+        double interset_rate;
+        double total_interset = 0;
+
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter investment amount: ");
+        money = scanner.nextDouble();
+        System.out.println("Enter number of months: ");
+        month = scanner.nextInt();
+        System.out.println("Enter annual interest rate in percentage: ");
+        interset_rate = scanner.nextDouble();
+
+        for(int i = 0; i < month; i++){
+            total_interset = money * (interset_rate/100)/12 * 3;
+        }
+        System.out.println("Total of interset: " + total_interset);
+    }
+}
