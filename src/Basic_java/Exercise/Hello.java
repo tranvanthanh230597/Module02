7984af1a99b5f2d81271d8f7907b524f80421f34
tranvanthanh230597 <tranvanthanh230597@gmail.com>
2019-10-11 16:56:08 +0700

    [Bài tập] Hiển thị lời chào

---
+++ src/Basic_java/Exercise/Hello.java
@@ -0,0 +1,11 @@
+package Basic_java.Exercise;
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
