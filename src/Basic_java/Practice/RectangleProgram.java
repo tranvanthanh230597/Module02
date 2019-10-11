8a9bd4b5d1d29276181868cd96550a20dbd09fd3
tranvanthanh230597 <tranvanthanh230597@gmail.com>
2019-10-11 16:45:38 +0700

    [Thực hành] Sử dụng toán tử

---
+++ src/Basic_java/Practice/RectangleProgram.java
@@ -0,0 +1,21 @@
+package Basic_java.Practice;
+
+import java.util.Scanner;
+
+public class RectangleProgram {
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+
+        float arena = width * height ;
+        System.out.println("Arena is: " + arena);
+    }
+}
