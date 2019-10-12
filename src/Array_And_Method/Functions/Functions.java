d248e0d20c263e28e066c240bdc088ad62548623
tranvanthanh230597 <tranvanthanh230597@gmail.com>
2019-10-12 19:58:41 +0700

    upload function hay dùng

---
+++ src/Array_And_Method/Functions/Functions.java
@@ -0,0 +1,16 @@
+package Array_And_Method.Functions;
+
+public class Functions {
+    public static int[] createMatrix(int num){
+        int[]arr = new int[num];
+        for (int i = 0 ; i <num ; i++){
+            arr[i] = (int)(Math.random()*100);
+        }
+        return arr;
+    }
+    public static void show(int[] arr){
+        for (int value : arr) {
+            System.out.print(value+" ");
+        }
+    }
+}
