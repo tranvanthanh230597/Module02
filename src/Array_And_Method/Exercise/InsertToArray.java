10bffa3eb8e12e772e49c371ad31d1daf610b4d1
tranvanthanh230597 <tranvanthanh230597@gmail.com>
2019-10-13 09:33:12 +0700

    sửa bài thêm phần tử vào mảng. chưa xong

--- src/Array_And_Method/Exercise/InsertToArray.java
+++ src/Array_And_Method/Exercise/InsertToArray.java
@@ -19,8 +19,21 @@ public class InsertToArray {
             System.out.print("Enter location in array you want insert: ");
             location = scanner.nextInt();
         }
-
         System.out.print("Enter value you want insert: ");
         int insertValue = scanner.nextInt();
+        int[]arr2nd = insertArray(arr,location,insertValue);
+        Functions.show(arr2nd);
+
+    }
+
+    public static int[] insertArray(int[]arr,int location, int insertValue){
+        int length = arr.length+1;
+        int[] arr2nd = new int[length];
+        System.arraycopy(arr, 0, arr2nd, 0, arr.length);
+        for (int i = arr2nd.length -1; i >= location; i--){
+            arr2nd[i+1] = arr2nd[i];
+        }
+        arr2nd[location]= insertValue;
+        return arr2nd;
     }
 }
