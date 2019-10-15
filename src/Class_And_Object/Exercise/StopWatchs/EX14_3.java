3f4a3331980b4dc1237678d95dd9e0e0ebc995f5
tranvanthanh230597 <tranvanthanh230597@gmail.com>
2019-10-15 15:45:56 +0700

    [Bài tập] Xây dựng lớp StopWatch

--- src/Class_And_Object/Exercise/StopWatchs/EX14_3.java
+++ src/Class_And_Object/Exercise/StopWatchs/EX14_3.java
@@ -7,9 +7,7 @@ public class EX14_3 {
         StopWatch watch = new StopWatch();
         watch.startTime();
         int[] arr = Functions.createMatrix(1000);
-        for (int i = 0; i < 1000; i++){
-            Functions.sortASC(arr);
-        }
+        Functions.show(Functions.sortASC(arr));
         watch.endTime();
         System.out.println("selection sort 1000 value has: " + watch.getElapsedTime() + " milisecond");
     }
