9a3eaf0d5c5580159f6fefd06689413fb947797a
tranvanthanh230597 <tranvanthanh230597@gmail.com>
2019-10-18 14:03:40 +0700

    [Thực hành] Hệ các đối tượng hình học

---
+++ src/Inheritance/Paratice/Shapes/Shape.java
@@ -0,0 +1,34 @@
+package Inheritance.Paratice.Shapes;
+
+public class Shape {
+    private String color = "green";
+    private boolean filled = false;
+
+    public String getColor() {
+        return color;
+    }
+    public boolean isFilled() {
+        return filled;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+    public Shape(){
+
+    }
+@Override
+    public String toString() {
+        return "A Shape with color of "
+                + getColor()
+                + " and "
+                + (isFilled() ? "filled" : "not filled");
+    }
+}
