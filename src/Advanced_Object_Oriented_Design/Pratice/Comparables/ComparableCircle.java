234bdf70884610c9187af160cad773c733974152
tranvanthanh230597 <tranvanthanh230597@gmail.com>
2019-10-22 10:26:31 +0700

    [**Thực hành] triển khai interface 'Comparable' cho các lớp hình học

---
+++ src/Advanced_Object_Oriented_Design/Pratice/Comparables/ComparableCircle.java
@@ -0,0 +1,23 @@
+package Advanced_Object_Oriented_Design.Pratice.Comparables;
+
+import java.util.Comparator;
+
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle> {
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color, boolean filled) {
+        super(radius, color, filled);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+}
