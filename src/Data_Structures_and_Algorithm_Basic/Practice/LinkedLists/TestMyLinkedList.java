c1809f3eeccda019027afddd16917ca0d9cf2fd3
tranvanthanh230597 <tranvanthanh230597@gmail.com>
2019-10-31 14:04:16 +0700

    [Thực hành] Triển khai lớp LinkedList đơn giản

---
+++ src/Data_Structures_and_Algorithm_Basic/Practice/LinkedLists/TestMyLinkedList.java
@@ -0,0 +1,14 @@
+package Data_Structures_and_Algorithm_Basic.Practice.LinkedLists;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+        ll.add(4,9);
+        ll.add(4,8);
+        ll.printList();
+    }
+}
